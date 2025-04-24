8dd5f6105666f6ee999d4a9e34e272231c592313
Quadeatschool <quadehafen@byui.edu>
2025-04-24 16:05:45 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        
+        Console.Write("Please input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write( $"Your first name is: {firstName}");
+
+
+        Console.Write("Please input your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine( $"Your first name is: {lastName} {firstName}");
+
+        int x = 10;
+        if (x == 9);
+
     }
+
+
 }
+
+
