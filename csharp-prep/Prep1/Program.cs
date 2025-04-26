f21288b35f8904d2136e7609fa42368d9a532626
Quadeatschool <quadehafen@byui.edu>
2025-04-26 11:57:43 -0600

    11:57 4/26

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Please input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine( $"Your first name is: {firstName}");
+
+
+        Console.Write("Please input your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine( $"Your name is {lastName}, {firstName} {lastName} ");
+
+        int x = 10;
+        if (x == 9);
+
     }
 }
