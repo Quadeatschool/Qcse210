d5a555914b76c7e691d50c673c30c2694f7b3876
Quadeatschool <quadehafen@byui.edu>
2025-04-26 14:10:32 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Enter your class score: ");
+        string score = Console.ReadLine();
+        int number = int.Parse(score);
+        string letter = "";
+
+        if (number >= 90)
+            {
+                letter = "A";
+            }
+        else if (number >= 80)
+            {
+                letter = "B";
+            }
+        else if (number >= 70)
+            {
+                letter = "C";
+            }
+        else if (number >= 60)
+            {
+                letter = "D";
+            }
+        else if (number < 60)
+            {
+                letter = "F";
+            }
+        else 
+            {
+                Console.WriteLine("Run that by me one more time? ");
+            }   
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+
+         if (number >= 70)
+            {
+                Console.WriteLine("Congrats, you have passed");
+            }
+        else 
+            {
+                Console.WriteLine("You have Failed");
+            }
     }
+       
 }
+
