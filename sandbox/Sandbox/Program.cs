ba3e6e0cee2a595df0a03513a43e9391764d71b1
Quadeatschool <quadehafen@byui.edu>
2025-04-29 09:36:01 -0600

    for loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,17 +7,25 @@ class Program
         Console.WriteLine("Hello Sandbox World!");
 
         
-        Console.Write("Please input your first name: ");
-        string firstName = Console.ReadLine();
-        Console.Write( $"Your first name is: {firstName}");
+        // Console.Write("Please input your first name: ");
+        // string firstName = Console.ReadLine();
+        // Console.Write( $"Your first name is: {firstName}");
 
 
-        Console.Write("Please input your last name: ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine( $"Your first name is: {lastName} {firstName}");
+        // Console.Write("Please input your last name: ");
+        // string lastName = Console.ReadLine();
+        // Console.WriteLine( $"Your first name is: {lastName} {firstName}");
 
         int x = 10;
         if (x == 9);
+        {
+            Console.WriteLine("X is 10");
+        }
+
+        for(int i = 0; i < x; i++)
+        {
+        Console.WriteLine($"bob is cool : {i}");
+        }
 
     }
 
