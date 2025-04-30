b0425e8738197dbaa3cae867d6d4d35a4c0d0d81
Quadeatschool <quadehafen@byui.edu>
2025-04-30 10:20:23 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -24,28 +24,36 @@ class Program
 
         // for (int magic_number = 6;)
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
+        int number = randomGenerator.Next(1, 100);
 
+        int guess = 0;
 
+        while (guess != number)
+        {
+            Console.WriteLine("Guess a number between 1 and 100");
+            guess = int.Parse(Console.ReadLine());
+        }
 
-        Console.WriteLine("Guess a number");
-        number = int.Parse(Console.ReadLine());
-
-        if (number == 6)
+        
+        if (guess == number)
         {
             Console.WriteLine("guessed it!");
         }
-        else if (number < 6)
+        else if (guess < number)
         {
             Console.WriteLine("Higher");
             Console.WriteLine("Guess a number");
             
         }
-        else
+        else if (guess > number)
         {
             Console.WriteLine("Lower");
             Console.WriteLine("Guess a number");
             
+        }
+        else 
+        {
+            Console.WriteLine("nice try poser");
         }
     }
 }
