808da24592e310fab9e997d98418a2759c92fa3e
Quadeatschool <quadehafen@byui.edu>
2025-05-02 16:05:53 -0600

    sum, average and largest

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -12,7 +12,7 @@ class Program
         int number = -1;
         while (number != 0 )
         {
-            Console.WriteLine("enter a number to be added to the number list: ");
+            Console.WriteLine("enter a number to be added to the number list, enter 0 when you are bored of the loop: ");
             number = int.Parse(Console.ReadLine());
             numbers.Add(number);
         }
@@ -24,6 +24,16 @@ class Program
         {
              Console.WriteLine(item);
         }
-        
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is {max}");
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The numbers in the list added together is {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average of the numbers in the list of numbers {average}");
+
+
     }
 }
