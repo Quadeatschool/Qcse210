be72aed4025c25b866e41dea8102f644db48e8dd
Quadeatschool <quadehafen@byui.edu>
2025-05-02 14:12:27 -0600

    while loop

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,6 +6,13 @@ class Program
     {
         Console.WriteLine("Hello Prep4 World!");
 
-        
+        List<int> numbers = new List<int>(); 
+        int number = -1;
+        while (number != 0 )
+        {
+            Console.WriteLine("enter a number to be added to the number list: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
     }
 }
