bc55903f3827d22b0c05debcecec86367e9550d0
Quadeatschool <quadehafen@byui.edu>
2025-05-15 09:58:12 -0600

    start menu

---
+++ prove/Develop02/menu.cs
@@ -0,0 +1,17 @@
+public class Menu
+{
+    public string _title;
+    public string _description;
+    public string _menuItem;
+
+    public void DisplayTitle()
+    {
+        Console.WriteLine($"{_title}");
+    }
+
+    public int ProcessMenu(_menuItem)
+    {
+        return ;
+   }
+    
+ }
