dbffae37a3cfc1d0dc6a9851804bac5b5f68cb16
Quadeatschool <quadehafen@byui.edu>
2025-05-22 11:02:10 -0600

    little fix

--- sandbox/Sandbox/Cylinder.cs
+++ sandbox/Sandbox/Cylinder.cs
@@ -1,22 +1,46 @@
-class Cylinder 
+
+using System.Globalization;
+
+class Cylinder
 {
     private Circle _circle;
 
     private double _height;
 
-    public void SetHeight(double height)
+    public Cylinder(Circle cirlce)
     {
-        _height = height;
+    _height = 0.0;
+    _circle = Circle;
     }
+
+    public Cylinder(double height, Circle circle)
+    {
+    _height = height;
+    _circle = circle;
+    }
+
     public void SetCircle(Circle circle)
     {
         _circle = circle;
+        SetHeight(10);
+    }
 
+    public void SetHeight(double height)
+    {
+        if (height < 0)
+        {
+            Console.WriteLine("Error, cannont set a negative value.");
+            return;
+        }
+        _height = height;
     }
 
     public double GetVolume()
     {
-        double radius = _circle.GetRadius();
-       return Math.PI * radius * radius * _height;
+        // double radius = _circle.GetRadius();
+        return _circle.GetArea() * _height;
+        // return Math.PI * radius * radius * _height;
+
     }
 }
+
