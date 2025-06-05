487c7fa049827fc1cb16ed41c77e8900549d2e10
Quadeatschool <quadehafen@byui.edu>
2025-06-05 11:38:36 -0600

    change

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,55 +1,55 @@
-// public class Scripture
-// {
-//     private string _textString;
-//     private Reference _reference;
+public class Scripture
+{
+    private string _textString;
+    private Reference _reference;
 
-//     private List<Word> _wordList;
+    private List<Word> _wordList;
 
-//     // Scripture Methods
+    // Scripture Methods
 
-//     public Scripture(List<Word> wordList, Reference reference, string textString)
-//     {
-//         _textString = textString;
-//         _reference = reference;
-//         _wordList = wordList;
+    public Scripture(List<Word> wordList, Reference reference, string textString)
+    {
+        _textString = textString;
+        _reference = reference;
+        _wordList = new List<Word>();
 
-//         wordList = new List<Word>();
+        foreach (string word in textString.Split(' '))
+        {
+            wordList.Add(new Word(word));
+        }
 
-//         foreach (string word in textString.Split(' '))
-//         {
-//             wordList.Add(new Word(word));
-//         }
+    }
+    public void DisplayScripture()
+    {
+        Console.WriteLine(_reference.DisplayReference());
 
-//     }
-//     public void DisplayScripture()
-//     {
-//         Console.WriteLine(_reference.DisplayReference());
+        foreach (Word word in _wordList)
+        {
+            Console.Write(word.GetWord() + " ");
+        }
+        Console.WriteLine();
+    }
 
-//         foreach (Word word in _wordList)
-//         {
-//             Console.Write(word.GetWord() + " ");
-//         }
-//         Console.WriteLine();
-//     }
+    public void ClearScreen()
+    {
+        // This will start by displaying "AAA" and waiting for the user to press the enter key
+        Console.WriteLine("AAA");
+        Console.ReadLine();
 
-//     public void ClearScreen()
-//     {
-//         // This will start by displaying "AAA" and waiting for the user to press the enter key
-//         Console.WriteLine("AAA");
-//         Console.ReadLine();
+        // This will clear the console
+        Console.Clear();
 
-//         // This will clear the console
-//         Console.Clear();
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
+    }
 
-//         // This will show "BBB" in the console where "AAA" used to be
-//         Console.WriteLine("BBB");
-//     }
-
-//     public string HideWords()
-//     {
-//         Random hid = new Random();
-//         int hidcount = 0;
+    public void HideWords(count =1)
+    // permanant paramater
+    {
+        Random rando = new Random();
+        var unhiddenWords = _wordList.Where(w => !w.IsHidden()).ToList();
+        int hidcount = 0;
 
 
-//     }
-// }
+    }
+}
