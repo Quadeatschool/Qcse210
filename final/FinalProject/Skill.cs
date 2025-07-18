97a1593fb5043c939c982d0eff456fac202deed3
Quadeatschool <quadehafen@byui.edu>
2025-07-17 23:07:09 -0600

    Update Skill.cs

--- final/FinalProject/Skill.cs
+++ final/FinalProject/Skill.cs
@@ -1,13 +1,15 @@
-public class Skill : SkillBase
+public class Skill : SkillBase //Inheritance
 {
     public Skill(string name, string description, bool isUnlocked, List<SkillBase> prerequisites) : base(name, description, isUnlocked, prerequisites)
     {
         
     }
 
+
+// Polymorphism
     public override bool CanUnlock(PlayerProgress progress)
     {
-        
+
         return GetPrerequisites().All(p => p.GetIsUnlocked());
     }
 }
